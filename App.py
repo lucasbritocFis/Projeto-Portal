532807bf4ba942fa9034953a6c5b5dec8ef70fac
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-04-07 15:10:47 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,41 +1,26 @@
 import streamlit as st
 import os
+
 st.set_page_config(page_title="Portal Interno", layout="centered")
 st.title("🖥️ Acesso Rápido - Portal Interno")
 
+# Citrix
 st.markdown("### 💻 Abrir Citrix StoreWeb")
-
-# URL do Citrix
 citrix_url = "http://bronbsv004app.adhosp.com.br/Citrix/StoreWeb/"
+st.markdown(f"[Abrir Citrix]({citrix_url})", unsafe_allow_html=True)
 
-# Botão com link
-st.markdown(f"""
-    <a href="{citrix_url}" target="_blank">
-        <button style='padding:10px 20px; font-size:16px; background-color:#4CAF50; color:white; border:none; border-radius:8px; cursor:pointer;'>
-            Abrir Citrix
-        </button>
-    </a>
-""", unsafe_allow_html=True)
-
+# Tasy
 st.markdown("### 💻 Abrir Tasy")
+tasy_url = "https://tasyprd.adhosp.com.br/#/login"
+st.markdown(f"[Abrir Tasy]({tasy_url})", unsafe_allow_html=True)
 
-# URL do Citrix
-citrix_url = "https://tasyprd.adhosp.com.br/#/login"
-
-# Botão com link
-st.markdown(f"""
-    <a href="{citrix_url}" target="_blank">
-        <button style='padding:10px 20px; font-size:16px; background-color:#4CAF50; color:white; border:none; border-radius:8px; cursor:pointer;'>
-            Abrir "Tasy
-        </button>
-    </a>
-""", unsafe_allow_html=True)
-
+# Abrir planilha Excel
+st.markdown("### 📊 Abrir Planilha Excel (rede interna)")
 excel_path = r"\\10.50.90.18\Radioterapia\PORTAL-RADIOTERAPIA.xlsb"
 
 if st.button("Abrir Planilha Excel"):
     try:
-        os.startfile(excel_path)
+        os.startfile(excel_path)  # Isso só funciona no Windows local
         st.success("Arquivo aberto com sucesso!")
     except Exception as e:
         st.error(f"Erro ao abrir o arquivo: {e}")
