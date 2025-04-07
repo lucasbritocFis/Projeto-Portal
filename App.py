f52f60dc90193720f3775404893e7a00416c5106
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2025-04-07 14:57:21 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.set_page_config(page_title="Portal Interno", layout="centered")
+st.title("🖥️ Acesso Rápido - Portal Interno")
+
+st.markdown("### 💻 Abrir Citrix StoreWeb")
+
+# URL do Citrix
+citrix_url = "https://bronbsv004app.adhosp.com.br/Citrix/StoreWeb/"
+
+# Botão com link
+st.markdown(f"""
+    <a href="{citrix_url}" target="_blank">
+        <button style='padding:10px 20px; font-size:16px; background-color:#4CAF50; color:white; border:none; border-radius:8px; cursor:pointer;'>
+            Abrir Citrix
+        </button>
+    </a>
+""", unsafe_allow_html=True)
